8b5ec5a4154026a14abcd565239a877cd4265717
gokhyk <gokhankula@yahoo.com>
2025-11-17 14:53:59 -0600

    Initial Commit

---
+++ USSoccerDad/USSoccerDadApp.swift
@@ -0,0 +1,17 @@
+//
+//  USSoccerDadApp.swift
+//  USSoccerDad
+//
+//  Created by Ayse Kula on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct USSoccerDadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ USSoccerDadTests/USSoccerDadTests.swift
@@ -0,0 +1,17 @@
+//
+//  USSoccerDadTests.swift
+//  USSoccerDadTests
+//
+//  Created by Ayse Kula on 11/17/25.
+//
+
+import Testing
+@testable import USSoccerDad
+
+struct USSoccerDadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
